16463f365bebeb312e5ca4aa4c8dd46cfb22ab2d
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-01-04 19:08:31 +0700

    Add monstrosity function

--- app/src/main/java/com/github/rifqimfahmi/profilingplayground/MainActivity.kt
+++ app/src/main/java/com/github/rifqimfahmi/profilingplayground/MainActivity.kt
@@ -1,12 +1,61 @@
 package com.github.rifqimfahmi.profilingplayground
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        small.setOnClickListener { executeSmallLoop() }
+        medium.setOnClickListener { executeMediumLoop() }
+        large.setOnClickListener { executeLargeLoop() }
+        enormous.setOnClickListener { executeEnormousLoop() }
+        monstrosity.setOnClickListener { executeMonstrosityLoop() }
+    }
+
+    private fun executeSmallLoop() {
+        for (i in 0..100) {
+            doNothing()
+        }
+        setLoopResult("Small Done")
+    }
+
+    private fun executeMediumLoop() {
+        for (i in 0..10_000) {
+            doNothing()
+        }
+        setLoopResult("Medium Done")
+    }
+
+    private fun executeLargeLoop() {
+        for (i in 0..1_000_000) {
+            doNothing()
+        }
+        setLoopResult("Large Done")
+    }
+
+    private fun executeEnormousLoop() {
+        for (i in 0..1_000_000_000) {
+            doNothing()
+        }
+        setLoopResult("Enormous Done")
+    }
+
+    private fun executeMonstrosityLoop() {
+        for (i in 0..1_000_000_000_000) {
+            doNothing()
+        }
+        setLoopResult("Monstrosity Done")
+    }
+
+    private fun setLoopResult(s: String) {
+        result.text = s
+    }
+
+    private fun doNothing() {
+
     }
 }
